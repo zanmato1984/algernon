39ef3108b39fed0df9d593f314bd9755773ac1f8
zanmato1984 <zanmato1984@gmail.com>
2018-12-03 18:44:38 +0800

    Support master (non-kafka) binlog

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf log data tmp
+rm -rf log/* data/*
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function print_usage() {
-  echo "[bin] -kafka-only/-no-binlog/-no-drainer/-all"
+  echo "[bin] -no-binlog/-no-drainer/-all"
 }
 
 if [ -z $1 ] || [ "$1" == "-all" ]; then
@@ -9,13 +9,11 @@ if [ -z $1 ] || [ "$1" == "-all" ]; then
     echo "You must specify env \$HOST_IP"
     exit 1
   fi
-  docker-compose up --scale tidb1=0
-elif [ "$1" == "-kafka-only" ]; then
-  docker-compose up --scale pd0=0 --scale tikv0=0 --scale pump0=0 --scale tidb0=0 --scale tidb1=0 --scale drainer0=0
+  docker-compose up --scale tidb-no-binlog0=0
 elif [ "$1" == "-no-binlog" ]; then
   docker-compose up --scale zk0=0 --scale kafka0=0 --scale pump0=0 --scale tidb0=0 --scale drainer0=0
 elif [ "$1" == "-no-drainer" ]; then
-  docker-compose up --scale tidb1=0 --scale drainer0=0
+  docker-compose up --scale tidb-no-binlog0=0 --scale drainer0=0
 else
   print_usage
   exit 1
