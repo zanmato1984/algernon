fb7ea7f838df9739da7acb35412d241e9d09c19a
zanmato1984 <zanmato1984@gmail.com>
2018-12-13 02:11:15 +0800

    Let status and stop concern learner

--- status.sh
+++ status.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watch -n 1 docker-compose ps
+watch -n 1 "docker-compose ps && docker-compose -f docker-compose-learner.yaml ps"
--- stop.sh
+++ stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+docker-compose -f docker-compose-learner.yaml down
 docker-compose down
