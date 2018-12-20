6c33a980d6bcf9618d4dd8a80f139a7f1cf30e84
zanmato1984 <zanmato1984@gmail.com>
2018-12-20 20:54:33 +0800

    Refine hw and ycsb

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function print_usage() {
-  echo "[bin] -raft [tikv/learner/tidb/go-ycsb/hw]/-pump/-binlog"
+  echo "[bin] -raft [tikv/learner/tidb/hw/ycsb]/-pump/-binlog"
 }
 
 if [ -z $1 ]; then
@@ -17,11 +17,11 @@ elif [ "$1" == "-raft" ]; then
     docker-compose exec pd0 /pd-ctl -d config set max-merge-region-value 0
   elif [ "$2" == "tidb" ]; then
     docker-compose -f docker-compose-tidb.yaml up -d
-  elif [ "$2" == "go-ycsb" ]; then
-    docker-compose -f docker-compose-go-ycsb.yaml run go-ycsb0 load mysql -P /go-ycsb.conf -p mysql.host=tidb0 -p mysql.port=4000 -p mysql.user=root
-    docker-compose -f docker-compose-go-ycsb.yaml run go-ycsb0 run mysql -P /go-ycsb.conf -p mysql.host=tidb0 -p mysql.port=4000 -p mysql.user=root
   elif [ "$2" == "hw" ]; then
     mysql -h 127.0.0.1 -P 4000 -u root -e "drop table if exists test.t; create table test.t(s varchar(256), i int); insert into test.t values('Hello world', 666)"
+  elif [ "$2" == "ycsb" ]; then
+    docker-compose -f docker-compose-go-ycsb.yaml run go-ycsb0 load mysql -P /go-ycsb.conf -p mysql.host=tidb0 -p mysql.port=4000 -p mysql.user=root
+    docker-compose -f docker-compose-go-ycsb.yaml run go-ycsb0 run mysql -P /go-ycsb.conf -p mysql.host=tidb0 -p mysql.port=4000 -p mysql.user=root
   else
     print_usage
     exit 1
