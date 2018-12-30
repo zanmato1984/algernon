a19330bc1543ee1a21e33a880eadb5d2610d8f0d
zanmato1984 <zanmato1984@gmail.com>
2018-12-30 20:21:06 +0800

    Cleanup scripts

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function print_usage() {
-  echo "[bin] -raft [tikv/learner/tidb/hw/ycsb]/-pump/-binlog"
+  echo "[bin] -raft [ycsb]/-pump/-binlog"
 }
 
 if [ -z $1 ]; then
@@ -9,19 +9,9 @@ if [ -z $1 ]; then
 elif [ "$1" == "-raft" ]; then
   if [ -z $2 ]; then
     docker-compose up -d --scale pump0=0 --scale tidb0=0 --scale tidb-binlog0=0 --scale drainer0=0
-  elif [ "$2" == "tikv" ]; then
-    docker-compose up -d --scale pump0=0 --scale tidb0=0 --scale tidb-raft0=0 --scale tidb-binlog0=0 --scale drainer0=0
-  elif [ "$2" == "learner" ]; then
-    docker-compose exec pd0 /pd-ctl -d op add add-learner 2 4
-    docker-compose exec pd0 /pd-ctl -d config set max-merge-region-keys 0
-    docker-compose exec pd0 /pd-ctl -d config set max-merge-region-value 0
-  elif [ "$2" == "tidb" ]; then
-    docker-compose -f docker-compose-tidb.yaml up -d
-  elif [ "$2" == "hw" ]; then
-    mysql -h 127.0.0.1 -P 4000 -u root -e "drop table if exists test.t; create table test.t(s varchar(256), i int); insert into test.t values('Hello world', 666)"
   elif [ "$2" == "ycsb" ]; then
-    docker-compose -f docker-compose-go-ycsb.yaml run go-ycsb0 load mysql -P /go-ycsb.conf -p mysql.host=tidb0 -p mysql.port=4000 -p mysql.user=root
-    docker-compose -f docker-compose-go-ycsb.yaml run go-ycsb0 run mysql -P /go-ycsb.conf -p mysql.host=tidb0 -p mysql.port=4000 -p mysql.user=root
+    docker-compose -f docker-compose-go-ycsb.yaml run go-ycsb0 load mysql -P /go-ycsb.conf -p mysql.host=tidb-raft0 -p mysql.port=4000 -p mysql.user=root
+    docker-compose -f docker-compose-go-ycsb.yaml run go-ycsb0 run mysql -P /go-ycsb.conf -p mysql.host=tidb-raft0 -p mysql.port=4000 -p mysql.user=root
   else
     print_usage
     exit 1
@@ -34,6 +24,8 @@ elif [ "$1" == "-binlog" ]; then
     exit 1
   fi
   docker-compose up -d --scale tikv-learner0=0 --scale tidb-raft0=0 --scale tidb0=0
+elif [ "$1" == "-hw" ]; then
+  mysql -h 127.0.0.1 -P 4000 -u root -e "drop table if exists test.t; create table test.t(s varchar(256), i int); insert into test.t values('Hello world', 666)"
 else
   print_usage
   exit 1
