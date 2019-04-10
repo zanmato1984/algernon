4a8715f41c203f47eaf3c82aed64ec49f3f6c22d
ruoxi <zanmato1984@gmail.com>
2019-04-10 18:40:19 +0800

    Raft to master (#3)
    * Add learner
    
    * Let status and stop concern learner
    
    * Recover start
    
    * WIP
    
    * WIP
    
    * Temporary 3 step raft startup
    
    * No explicit timeout
    
    * Separate TiDB
    
    * Change engine addr to linux default
    
    * Refine raft stuff
    
    * Refine hw and ycsb
    
    * Add label in pd and tikv
    
    * Fix engine in tikv learner config
    
    * Use new pd/learner image
    
    * Add pd/tikv config
    
    * Cleanup scripts
    
    * Update to 190107
    
    * Update start script
    
    * Update to latest images
    
    * Fix leader image
    
    * Update image
    
    * Shorten ycsb scale
    
    * Remove tidb-raft
    
    * Fix docker file
    
    * Fix tidb image tag as 2.1.4
    
    * Update tidb image
    
    *  Enable tiflash docker and remove all useless ones (#2)
    
    * Remove all useless ones
    
    * Fix tikv
    
    * Remove some confis
    
    * Enable tiflash docker
    
    * Change tics image tag
    
    * Upgrade tidb version
    
    * Add debug for raft
    
    * Enhance test scripts (ycsb and query)
    
    * Enhance tiflash config
    
    * Fix tiflash config

--- start.sh
+++ start.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 function print_usage() {
-  echo "[bin] -no-binlog/-no-drainer/-all"
+  echo "./start.sh [-raft]"
 }
 
-if [ -z $1 ] || [ "$1" == "-all" ]; then
-  if [ -z $HOST_IP ]; then
-    echo "You must specify env \$HOST_IP"
+if [ -z $1 ]; then
+  docker-compose up -d --scale tiflash0=0 --scale tikv-learner0=0
+elif [ "$1" == "-raft" ]; then
+  if [ -z $2 ]; then
+    docker-compose up -d
+  elif [ "$2" == "debug" ]; then
+    docker-compose up -d --scale tiflash0=0
+  else
+    print_usage
     exit 1
   fi
-  docker-compose up --scale tidb-no-binlog0=0
-elif [ "$1" == "-no-binlog" ]; then
-  docker-compose up --scale pump0=0 --scale tidb0=0 --scale drainer0=0
-elif [ "$1" == "-no-drainer" ]; then
-  docker-compose up --scale tidb-no-binlog0=0 --scale drainer0=0
 else
   print_usage
   exit 1
---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function print_usage() {
+  echo "./test.sh [hw/ycsb/mysql-q query/tiflash-q query]"
+}
+
+MYSQL_HOST="192.168.65.2"
+
+if [ -z "$1" ]; then
+  print_usage
+  exit 1
+elif [ "$1" == "hw" ]; then
+  mysql -h 127.0.0.1 -P 4000 -u root -e "drop table if exists test.t; create table test.t(s varchar(256), i int); insert into test.t values('Hello world', 666)"
+elif [ "$1" == "ycsb" ]; then
+  docker run --rm pingcap/go-ycsb load mysql -p mysql.host=$MYSQL_HOST -p mysql.port=4000 -p mysql.user=root -p recordcount=10000 -p operationcount=1000
+  docker run --rm pingcap/go-ycsb run mysql -p mysql.host=$MYSQL_HOST -p mysql.port=4000 -p mysql.user=root -p recordcount=10000 -p operationcount=1000
+elif [ "$1" == "mysql-q" ]; then
+  if [ -z "$2" ]; then
+    print_usage
+    exit 1
+  else
+    mysql -h 127.0.0.1 -P 4000 -u root -e "$2"
+  fi
+elif [ "$1" == "tiflash-q" ]; then
+  if [ -z "$2" ]; then
+    print_usage
+    exit 1
+  else
+    docker-compose exec tiflash0 ./theflash client --host="127.0.0.1" --query="$2"
+  fi
+else
+  print_usage
+  exit 1
+fi
