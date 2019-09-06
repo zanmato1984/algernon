534b0546d1500ac4e815837cc55c9269bed3c3c2
zanmato1984 <zanmato1984@gmail.com>
2019-09-07 03:54:58 +0800

    Dynamic relocate engine addr in tikv learner config

--- start.sh
+++ start.sh
@@ -8,9 +8,9 @@ if [ -z $1 ]; then
   docker-compose up -d --scale tiflash-gateway0=0 --scale tiflash0=0 --scale tikv-learner0=0
 elif [ "$1" == "-raft" ]; then
   if [ -z $2 ]; then
-    docker-compose up -d
+    docker-compose up -d --build
   elif [ "$2" == "debug" ]; then
-    docker-compose up -d --scale tiflash0=0
+    docker-compose up -d --scale tiflash0=0 --build
   else
     print_usage
     exit 1
