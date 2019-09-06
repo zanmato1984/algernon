4a8686a90bb92ff88942d6ce3f387c6a1d877e09
zanmato1984 <zanmato1984@gmail.com>
2019-09-07 04:15:44 +0800

    Automate start by setting addresses according to OS and mode properly

--- start.sh
+++ start.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+set -ue
+
 function print_usage() {
   echo "./start.sh [-raft]"
 }
 
+# Mac docker uses IP of host.docker.internal, likely as the following, but not for 100% sure.
+MAC_DOCKER_IP="192.168.65.2"
+# Linux docker uses fixed IP as following.
+LINUX_DOCKER_IP="172.17.0.1"
+
 if [ -z $1 ]; then
-  docker-compose up -d --scale tiflash-gateway0=0 --scale tiflash0=0 --scale tikv-learner0=0
+  docker-compose up -d --scale tiflash0=0 --scale tiflash-gateway0=0 --scale tikv-learner0=0
 elif [ "$1" == "-raft" ]; then
   if [ -z $2 ]; then
     docker-compose up -d --build
   elif [ "$2" == "debug" ]; then
+    if [ `uname` == "Darwin" ]; then
+      export FLASH_ADDR=${MAC_DOCKER_IP}
+      export ENGINE_ADDR=${MAC_DOCKER_IP}
+      echo "Mac docker detected, using FLASH_ADDR as $FLASH_ADDR and ENGINE_ADDR as $ENGINE_ADDR."
+    else
+      export FLASH_ADDR=${LINUX_DOCKER_IP}
+      echo "Linux docker detected, using FLASH_ADDR as $FLASH_ADDR."
+    fi
     docker-compose up -d --scale tiflash0=0 --build
   else
     print_usage
