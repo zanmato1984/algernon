72c4554f4d2bc58165810ada004939dfb9a0e2b5
ruoxi <zanmato1984@gmail.com>
2019-10-21 17:51:12 +0800

    Add coprocessor support in algernon (#4)
    * make it working with cop branch
    
    * Use cop tidb
    
    * Change tics image
    
    * Recover setting
    
    * Link tikv-learner0 to tiflash0 to redirect tidb cop target
    
    * Use gateway to route learner to tiflash
    
    * Dynamic relocate engine addr in tikv learner config
    
    * Automate start by setting addresses according to OS and mode properly
    
    * Fix unbind variable
    
    * Set batch size to 0 to workaround batch command
    
    * Enable batch
    
    * Update config
    
    * Enhance cop branch

--- start.sh
+++ start.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+set -e
+
+source env.sh
+
 function print_usage() {
-  echo "./start.sh [-raft]"
+  echo "./start.sh [-raft [debug] | -docker start/restart/stop/...]"
 }
 
 if [ -z $1 ]; then
-  docker-compose up -d --scale tiflash0=0 --scale tikv-learner0=0
+  docker-compose up -d --scale tidb1=0 --scale tiflash0=0 --scale tiflash-gateway0=0 --scale tikv-learner0=0
 elif [ "$1" == "-raft" ]; then
   if [ -z $2 ]; then
-    docker-compose up -d
+    docker-compose up -d --scale tidb0=0 --build
   elif [ "$2" == "debug" ]; then
-    docker-compose up -d --scale tiflash0=0
+    docker-compose up -d --scale tidb0=0 --scale tiflash0=0 --build
   else
     print_usage
     exit 1
   fi
+elif [ "$1" == "-docker" ]; then
+  docker-compose "${@:2}"
 else
   print_usage
   exit 1
