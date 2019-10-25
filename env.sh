36a6bf83a73a0ca8dbc4480af200ce1e3493ead7
zanmato1984 <zanmato1984@gmail.com>
2019-10-25 18:16:49 +0800

    Use host name for tiflash0

--- env.sh
+++ env.sh
@@ -8,13 +8,11 @@ MAC_DOCKER_IP="192.168.65.2"
 LINUX_DOCKER_IP="172.17.0.1"
 
 if [ `uname` == "Darwin" ]; then
-  export ENGINE_ADDR=${MAC_DOCKER_IP}
   export MYSQL_HOST=${MAC_DOCKER_IP}
   echo "Mac docker detected."
 else
-  export ENGINE_ADDR=${LINUX_DOCKER_IP}
   export MYSQL_HOST=${LINUX_DOCKER_IP}
   echo "Linux docker detected."
 fi
 
-echo "Using ENGINE_ADDR as $ENGINE_ADDR, MYSQL_HOST as $MYSQL_HOST."
+echo "Using MYSQL_HOST as $MYSQL_HOST."
