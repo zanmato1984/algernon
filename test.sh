f2fa77a2fd702227babcfa238f56f71d4e31c101
zanmato1984 <zanmato1984@gmail.com>
2019-10-25 15:50:46 +0800

    Add pd config of replacement rule and add ddl to add tiflash replica to hw

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ if [ -z "$1" ]; then
   print_usage
   exit 1
 elif [ "$1" == "hw" ]; then
-  mysql -h 127.0.0.1 -P 4000 -u root -e "drop table if exists test.t; create table test.t(s varchar(256), i int); insert into test.t values('Hello world', 666)"
+  mysql -h 127.0.0.1 -P 4000 -u root -e "drop table if exists test.t; create table test.t(s varchar(256), i int); insert into test.t values('Hello world', 666); alter table test.t set tiflash replica 1 location labels 'rack', 'host', 'abc';"
 elif [ "$1" == "ycsb" ]; then
   docker run --rm pingcap/go-ycsb load mysql -p mysql.host=$MYSQL_HOST -p mysql.port=4000 -p mysql.user=root -p recordcount=10000 -p operationcount=1000
   docker run --rm pingcap/go-ycsb run mysql -p mysql.host=$MYSQL_HOST -p mysql.port=4000 -p mysql.user=root -p recordcount=10000 -p operationcount=1000
