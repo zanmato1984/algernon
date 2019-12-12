d663b840b006ad9031e17285673b52c3eab111dd
zanmato1984 <zanmato1984@gmail.com>
2019-12-12 18:45:06 +0800

    Support 3 in 1

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ function print_usage() {
 }
 
 if [ -z $1 ]; then
-  docker-compose up -d --scale tiflash-gateway0=0 --scale tiflash0=0 --scale tikv-learner0=0 --scale tiflash-cluster-manager0=0
+  docker-compose up -d --scale tiflash0=0
 elif [ "$1" == "tiflash" ]; then
   if [ -z $2 ]; then
     docker-compose up -d
