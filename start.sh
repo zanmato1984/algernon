a6165ca2bbd9cc45f84b2610455e33735dfe3117
zanmato1984 <zanmato1984@gmail.com>
2020-01-03 19:06:10 +0800

    Support debug

--- start.sh
+++ start.sh
@@ -9,10 +9,11 @@ function print_usage() {
 }
 
 if [ -z $1 ]; then
-  docker-compose up -d --scale tiflash0=0
+  docker-compose up -d --scale tiflash-gateway0=0 --scale tiflash0=0
+  mysql -h 127.0.0.1 -P 4000 -u root -e "set @@global.tidb_row_format_version=1;"
 elif [ "$1" == "tiflash" ]; then
   if [ -z $2 ]; then
-    docker-compose up -d
+    docker-compose up -d --scale tiflash-gateway0=0
   elif [ "$2" == "-debug" ]; then
     export_tiflash_addr
     docker-compose up -d --scale tiflash0=0
