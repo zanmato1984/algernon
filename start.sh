dd18741cdf649e3c66941ff9b36129bcc660f13e
zanmato1984 <zanmato1984@gmail.com>
2020-02-04 12:10:35 +0800

    Support new row format

--- start.sh
+++ start.sh
@@ -10,7 +10,6 @@ function print_usage() {
 
 if [ -z $1 ]; then
   docker-compose up -d --scale tiflash-gateway0=0 --scale tiflash0=0
-  mysql -h 127.0.0.1 -P 4000 -u root -e "set @@global.tidb_row_format_version=1;"
 elif [ "$1" == "tiflash" ]; then
   if [ -z $2 ]; then
     docker-compose up -d --scale tiflash-gateway0=0
